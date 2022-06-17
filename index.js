8862268a410814c55c7e27dcf0c9ce0fac3c5ecf
Seth David Gyimah <esec26@gmail.com>
2022-06-17 08:47:31 +0000

    domcontrolled

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+
+  });
+  
+  
+  
